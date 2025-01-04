7e4a4dd598da7b3210df162eb4b6ce78b5b5561c
segundoinquilino <fabiolimagutierrez@gmail.com>
2025-01-04 15:14:38 -0300

    Fix: add router import in ReviewForm

--- components/ReviewForm.tsx
+++ components/ReviewForm.tsx
@@ -1,3 +1,15 @@
-const onSuccess = () => {
-  router.push('/home') // Redireciona para home após sucesso
-} 
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+const ReviewForm = () => {
+  const router = useRouter()
+  
+  const onSuccess = () => {
+    router.push('/home')
+  }
+  
+  // ... resto do código
+}
+
+export default ReviewForm 
