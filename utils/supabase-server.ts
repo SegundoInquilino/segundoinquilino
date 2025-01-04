eb63616bfa7662d1a41e9bebe993402c311d6cce
segundoinquilino <fabiolimagutierrez@gmail.com>
2025-01-04 15:19:18 -0300

    Fix: remove manual supabase config in server client

--- utils/supabase-server.ts
+++ utils/supabase-server.ts
@@ -1,9 +1,8 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { Database } from '@/types/supabase'
 
-export const createServerClient = () =>
-  createServerComponentClient({
-    cookies,
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+export const createClient = () =>
+  createServerComponentClient<Database>({
+    cookies
   }) 
