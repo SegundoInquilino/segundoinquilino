24a8cd737fbd6810cca46a7e8fe19c8a00d40141
segundoinquilino <fabiolimagutierrez@gmail.com>
2025-01-21 15:22:50 -0300

    feat: moderniza interface e corrige configurações
    Interface:
    - Adiciona ícone de calendário no período de moradia
    - Reposiciona informações de período nos cards
    - Implementa AlertDialog do Radix UI para confirmações
    - Melhora feedback visual nas ações
    
    Maps:
    - Adiciona suporte a latitude/longitude
    - Implementa coordenadas padrão
    - Melhora exibição de marcadores
    - Otimiza lógica de coordenadas
    
    Build:
    - Simplifica next.config.js
    - Remove configurações desnecessárias do webpack
    - Corrige dependências do Radix UI
    - Melhora organização do código

--- next.config.js
+++ next.config.js
@@ -7,14 +7,7 @@ const nextConfig = {
     '@radix-ui/react-dialog',
     '@radix-ui/react-primitive',
     '@heroicons/react'
-  ],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@radix-ui/react-alert-dialog': require.resolve('@radix-ui/react-alert-dialog')
-    }
-    return config
-  }
+  ]
 }
 
 module.exports = nextConfig 
